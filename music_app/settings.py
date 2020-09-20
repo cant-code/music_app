5fe16f140119e13175dec98a46fd254bf5a04b0e
Ayush <ayushc13000@gmail.com>
2020-09-20 23:50:24 +0530

    Test Production

--- music_app/settings.py
+++ music_app/settings.py
@@ -148,7 +148,7 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
