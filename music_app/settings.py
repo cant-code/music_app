c9445b86ba67c3fe7e8fc24bd48cc72b48ca8e77
Ayush <ayushc13000@gmail.com>
2020-09-20 22:45:37 +0530

    Test Production

--- music_app/settings.py
+++ music_app/settings.py
@@ -29,9 +29,9 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['music-mixin.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
