fbc256dcf90c33c922efb1ec93a796cd3acda394
Ayush <ayushc13000@gmail.com>
2020-09-23 16:44:20 +0530

    Url Fix

--- spotify/urls.py
+++ spotify/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
     path('auth/', views.DefineToken.as_view(), name='auth'),
     path('get_data/', views.SpotifyDataView.as_view(), name='get_data'),
     path('refresh_token/', views.RefreshToken.as_view(), name='refresh_token'),
-    path('player/', views.PlayPause.as_view(), name='player'),
-    path('player/play/', views.play),
-    path('player/pause/', views.pause)
 ]
