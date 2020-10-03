56553927d9a89cc670e651305582d019eb238d91
Ayush <ayushc13000@gmail.com>
2020-10-03 08:56:46 +0530

    model

--- spotify/models.py
+++ spotify/models.py
@@ -7,11 +7,11 @@ User = get_user_model()
 
 class SpotifyData(models.Model):
     user = models.ForeignKey(User, related_name='spotifydata', on_delete=models.CASCADE)
-    sp_name = models.CharField(primary_key=True, max_length=256)
+    sp_name = models.CharField(primary_key=True)
     access_token = models.CharField()
     refresh_token = models.CharField()
     expires_at = models.BigIntegerField()
-    category = models.CharField(max_length=20)
+    category = models.CharField()
 
     def __str__(self):
         return self.sp_name
