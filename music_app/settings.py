f684f0fae91e55b452697463c47c09f9a136b644
cant-code <ayushc13000@gmail.com>
2023-08-20 18:05:25 +0530

    Updated packages

--- music_app/settings.py
+++ music_app/settings.py
@@ -91,7 +91,16 @@ WSGI_APPLICATION = 'music_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "music",
+        "USER": "postgres",
+        "PASSWORD": "pass",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
