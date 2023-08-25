79dab1f1f2c48beb12d11962e0bbd53035789a7e
cant-code <ayushc13000@gmail.com>
2023-08-25 09:41:37 +0530

    Fixed security issue with get csrf token API

--- music_app/views.py
+++ music_app/views.py
@@ -1,14 +1,14 @@
 from django.http import JsonResponse, HttpResponse
 from django.views.generic import TemplateView
 import django.middleware.csrf
+from rest_framework.decorators import api_view
 
 
 class HomeView(TemplateView):
     template_name = 'index.html'
 
 
+@api_view(['GET'])
 def get_csrf_token(request):
-    if request.method == 'GET':
-        token = django.middleware.csrf.get_token(request)
-        return JsonResponse({'token': token})
-    return HttpResponse(status=405)
+    token = django.middleware.csrf.get_token(request)
+    return JsonResponse({'token': token})
